3e995714df66418d0f9667ce0618f6811aee153b
v00000033 <20746351@qq.com>
2022-05-20 18:18:19 +0800

    redis

--- redis/src/test/java/com/example/redis/base/StringTests.java
+++ redis/src/test/java/com/example/redis/base/StringTests.java
@@ -1,6 +1,8 @@
 package com.example.redis.base;
 
+import com.example.redis.redission.RedisBase;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -12,9 +14,25 @@ class StringTests {
     @Autowired
     StringRedisTemplate redisTemplate;
 
+    @Autowired
+    RedisBase redisBase;
+
+    @Autowired
+    RedissonClient redissonClient;
     @Test
     void contextLoads() {
         redisTemplate.opsForValue().set("sqs", "sqs");
+    }
+
+    @Test
+    void contextLoads1() {
+        redisBase.setKey("manjun", "123");
+    }
+
+    @Test
+    void contextLoads2() {
+        Object manjun = redissonClient.getBucket("manjun").get();
+        System.out.println(manjun);
     }
 
 }
