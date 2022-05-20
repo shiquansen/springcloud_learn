02b5d0da1cb9da9e1e619907be26219d81baf1e2
v00000033 <20746351@qq.com>
2022-05-20 18:18:52 +0800

    redis

---
+++ redis/src/main/java/com/example/redis/redission/RedisBase.java
@@ -0,0 +1,22 @@
+package com.example.redis.redission;
+
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisClient;
+import org.redisson.client.protocol.RedisCommands;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisBase {
+
+    @Autowired
+    RedissonConnectionFactory redissonConnectionFactory;
+
+    @Autowired
+    RedissonClient redissonClient;
+
+    public void setKey(String key, String value){
+        redissonClient.getBucket(key).set(value);
+    }
+}
